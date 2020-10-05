4f7df3b8ec26fb79993d0963b8c998bbf51280da
NareshGarg99 <42374606+NareshGarg99@users.noreply.github.com>
2020-10-05 17:59:01 +0530

    v2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser : true})
+mongoose.connect("mongodb+srv://admin-naresh:C7qJ5caufKq5AwK@cluster1.gbu9u.mongodb.net/todolistDB", {useNewUrlParser : true, useUnifiedTopology: true})
 
 const itemsSchema = {
   name : String
